014a3c1035fbd0b72150169865941c8880a234a7
Minh Phuong <dominhphuong2208@gmail.com>
2020-10-02 10:47:30 +0700

    update first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit,:update]
+
+
     def new 
         @user = User.new
     end
@@ -22,12 +25,27 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
     def update
-        
+        @user = User.find(params[:id])
+        if @user.update(users_params)
+            flash.now[:success] = "Successfully updated"
+            redirect_to @user
+        else
+            flash.now[:danger] = "Update Failed"
+            render :edit
+        end
     end
 
     private
 
     def users_params
         params.require(:user).permit(:username,:email,:password,:password_confirmation)
+    end
+
+
+    def logged_in_user
+        unless logged_in?
+            flash[:danger] = "Please login!"
+            redirect_to login_path
+        end
     end
 end
